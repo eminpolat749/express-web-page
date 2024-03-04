d3ac953732658522a71bcd088162d7a3c2d3b595
Emin Polat <eminpolat749@gmail.com>
2024-03-04 17:22:35 +0300

    done

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("locationbtn").onclick = function() {
+    window.location.href = "https://www.google.com/maps/dir/36.7689728,34.553856/express+oto+y%C4%B1kama/@36.9026312,34.2338592,9z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x152887cc28f31193:0x19ad050f0efc8f00!2m2!1d35.2510589!2d36.9957483?entry=ttu";
+};
+
